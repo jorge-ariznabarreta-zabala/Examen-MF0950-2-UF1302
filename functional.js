55b3428b8a0c6294aa5e77f0fdd74feebde36233
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-07-05 10:57:05 +0200

    Cambia foto hover

--- functional.js
+++ functional.js
@@ -1,27 +1,28 @@
 //Escribir la fecha en #date
-let cajaFecha=document.getElementById("date");
-let fechaHoy= new Date();
- console.log(fechaHoy.toLocaleDateString('es-ES'));
-let fecha=fechaHoy.toLocaleDateString('es-ES');
-const dias = [
-    'Dom',
-    'Lun',
-    'Mar',
-    'Mie',
-    'Jue',
-    'Vie',
-    'Sab',
-  ];
-  const numeroDia = fechaHoy.getDay();
-  const nombreDia = dias[numeroDia];
-  cajaFecha.innerText= `${nombreDia}, ${fecha}`
+let cajaFecha = document.getElementById('date');
+let fechaHoy = new Date();
+console.log(fechaHoy.toLocaleDateString('es-ES'));
+let fecha = fechaHoy.toLocaleDateString('es-ES');
+const dias = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
+const numeroDia = fechaHoy.getDay();
+const nombreDia = dias[numeroDia];
+cajaFecha.innerText = `${nombreDia}, ${fecha}`;
 
-  //Checkbox para activar Submit
-  let checkbox= document.getElementById("privacity")
-  let submitButton= document.getElementById("submitButton")
-  checkbox.addEventListener("change", buttonEnabler)
-  function buttonEnabler(){
-    console.log(submitButton.disabled);
-    console.log(checkbox.checked);
-    submitButton.disabled=!checkbox.checked
+//Checkbox para activar Submit
+let checkbox = document.getElementById('privacity');
+let submitButton = document.getElementById('submitButton');
+checkbox.addEventListener('change', buttonEnabler);
+function buttonEnabler() {
+	console.log(submitButton.disabled);
+	console.log(checkbox.checked);
+	submitButton.disabled = !checkbox.checked;
 }
+
+//Cambio de imagenes
+let cajaCambio=document.querySelector("#paraCambiar");   
+cajaCambio.addEventListener("mouseover", (event)=>{
+    cajaCambio.setAttribute("src", "assets/Foto1.jpg");
+});
+cajaCambio.addEventListener("mouseleave", (event)=>{
+    cajaCambio.setAttribute("src", "assets/Hidrogeno_Verde_746x419.jpg")
+});
